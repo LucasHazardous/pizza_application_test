901e73b452ced707d9ee4d9e4463622a95d2db16
Adam W <developeradamw@gmail.com>
2022-05-18 13:25:36 +0200

    pizza, size, menu dto

---
+++ src/main/java/hazardous/lucas/pizzaapplicationtest/remote/rest/dto/response/MenuDto.java
@@ -0,0 +1,21 @@
+package hazardous.lucas.pizzaapplicationtest.remote.rest.dto.response;
+
+import java.util.List;
+
+public class MenuDto {
+    public List<PizzaDto> pizzas;
+
+    public MenuDto() {}
+
+    public MenuDto(List<PizzaDto> pizzas) {
+        this.pizzas = pizzas;
+    }
+
+    public List<PizzaDto> getPizzas() {
+        return pizzas;
+    }
+
+    public void setPizzas(List<PizzaDto> pizzas) {
+        this.pizzas = pizzas;
+    }
+}
