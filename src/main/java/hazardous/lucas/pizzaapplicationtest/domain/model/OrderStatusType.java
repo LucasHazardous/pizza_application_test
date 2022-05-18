7e99a69557e3d1492707ab3057cd75f69c28dec1
Adam W <developeradamw@gmail.com>
2022-05-18 13:19:26 +0200

    enums

---
+++ src/main/java/hazardous/lucas/pizzaapplicationtest/domain/model/OrderStatusType.java
@@ -0,0 +1,5 @@
+package hazardous.lucas.pizzaapplicationtest.domain.model;
+
+public enum OrderStatusType {
+    IN_PROGRESS, READY, CANCELLED, DELIVERY, NEW
+}
---
+++ src/main/java/hazardous/lucas/pizzaapplicationtest/domain/model/SizeType.java
@@ -0,0 +1,5 @@
+package hazardous.lucas.pizzaapplicationtest.domain.model;
+
+public enum SizeType {
+    S, M, L
+}
